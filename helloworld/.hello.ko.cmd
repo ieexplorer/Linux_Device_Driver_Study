b6b53a2e2c8a1bf1d77624d28a3548e9ece9987a
Xu Zhengyu <ieexplorer@hotmail.com>
2018-08-17 18:36:05 +0800

    clean repo

--- helloworld/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alex/LDD/helloworld/hello.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/alex/LDD/helloworld/hello.ko /home/alex/LDD/helloworld/hello.o /home/alex/LDD/helloworld/hello.mod.o ;  true
